4bc5ac17741cb5ca27a8ce920134fe2a10105678
Le Vu <levu08072002@gmail.com>
2022-03-08 16:50:46 +0700

    add expressjs

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+// app.get('/',(req, res) => {
+//     res.send('Hello World')
+//   })
+// app.get("/api/products",(req, res) => {
+//     const data = [
+//    {id:1, name: "Prodcut A"},
+//    {id:2, name: "Prodcut b"}
+//     ]
+//     res.json(data)
+// })
+app.use(cors())
+app.get("/api/users",(req, res) => {
+    const data = [
+        {id:1, name: "User 1"},
+        {id:2, name: "User 2"},
+        {id:3, name: "User 3"},
+    ]
+    res.json(data)
+})
+
+
+app.listen(3001)
